16a3756574930f5c58f13e625629d525d96556cf
ylomri <56874717+ylomri@users.noreply.github.com>
2022-11-03 15:24:08 +0100

    Debug 3

--- meecgates.py
+++ meecgates.py
@@ -1,52 +1,13 @@
 
-from flask import Flask, render_template, request, redirect, url_for, send_file, session
-from datetime import datetime, timedelta
-import requests
-import random
-import string
-import flask_restful
-from flask_restful import Api
-from flask_cors import CORS
+from flask import *
+
 
 app = Flask(__name__)
-api = Api(app)
-CORS(app)
 
-
-@app.route("")
+@app.route("/")
 def home():
 
     return render_template('base.html')
-
-@app.context_processor
-def utility_processor():
-    def pluralize(count, singular, plural=None):
-        if not isinstance(count, int):
-            raise ValueError('"{}" must be an integer'.format(count))
-
-        if plural is None:
-            plural = singular + 's'
-
-        string = singular if count == 1 else plural
-
-        return "{} {}".format(count, string)
-
-    return dict(pluralize=pluralize, now=datetime.now())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ != '__main__':
